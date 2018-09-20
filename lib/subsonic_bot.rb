79ba8b9990b498804831bb3d02e5d781dcdbdc81
ausomator <rfox75@gmail.com>
2018-09-20 15:34:23 -0600

    updated help

--- lib/subsonic_bot.rb
+++ lib/subsonic_bot.rb
@@ -115,7 +115,8 @@ module SubsonicBot
       help_string += "subsonic song (optional: song name)\n"
       help_string += "subsonic album (optional: artist)\n"
       help_string += "subsonic artist (optional: artist)\n"
-      help_string += "subsonic song Evolution"
+      help_string += "subsonic song Evolution\n\n"
+      help_string += 'Login here "#{ENV['SUBSONIC_SERVER']}" with the username "#{ENV['USERNAME']}" to listen in!"
     end
 
     def slack_response_hash(output)
